ff7196bbe9c022da7ab3e2f8ba9c1c4263b53ed0
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-08-31 19:34:37 -0300

    feat: organização do repositório

---
+++ src/main/java/exercicioTesteUnitario/Pessoa.java
@@ -0,0 +1,82 @@
+package exercicioTesteUnitario;
+
+public class Pessoa {
+
+    private float peso;
+    private float altura;
+    private String sexo;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        if (!(sexo.equals("Masculino") || sexo.equals("Feminino"))){
+            throw new IllegalArgumentException("Sexo Inválido");
+        }
+        this.sexo = sexo;
+    }
+
+    public float calcularImc() {
+        return peso / (this.altura * this.altura);
+    }
+
+    public String verificarImc() {
+        float imc;
+        imc = this.calcularImc();
+        if (this.sexo.equals("Masculino")) {
+            if (imc < 20.7) {
+                return "Abaixo do peso";
+            } else {
+                if (imc <= 26.4) {
+                    return "No peso normal";
+                } else {
+                    if (imc < 27.8) {
+                        return "Marginalmente acima do peso";
+                    } else {
+                        if (imc <= 31.1) {
+                            return "Acima do peso ideal";
+                        } else {
+                            return "Obeso";
+                        }
+                    }
+                }
+            }
+        } else {
+            if (imc < 19.1) {
+                return "Abaixo do peso";
+            } else {
+                if (imc <= 25.8) {
+                    return "No peso normal";
+                } else {
+                    if (imc < 27.3) {
+                        return "Marginalmente acima do peso";
+                    } else {
+                        if (imc <= 32.3) {
+                            return "Acima do peso ideal";
+                        } else {
+                            return "Obeso";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ src/test/java/exercicioTesteUnitario/PessoaTest.java
@@ -0,0 +1,60 @@
+package exercicioTesteUnitario;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PessoaTest {
+
+    Pessoa pessoa;
+
+    @BeforeEach
+    void setUp() {
+        pessoa = new Pessoa();
+    }
+
+    @Test
+    public void testMulherAbaixoDoPeso() {
+        pessoa.setPeso(45.0f);
+        pessoa.setAltura(1.70f);
+        pessoa.setSexo("Masculino");
+        assertEquals("Abaixo do peso", pessoa.verificarImc());
+    }
+
+    @Test
+    public void testMulherPesoNormal() {
+        pessoa.setPeso(60.0f);
+        pessoa.setAltura(1.65f);
+        pessoa.setSexo("Masculino");
+        assertEquals("No peso normal", pessoa.verificarImc());
+    }
+
+    @Test
+    public void testHomemAcimaDoPeso() {
+        pessoa.setPeso(90.0f);
+        pessoa.setAltura(1.75f);
+        pessoa.setSexo("Masculino");
+        assertEquals("Acima do peso ideal", pessoa.verificarImc());
+    }
+
+    @Test
+    public void testHomemObeso() {
+        pessoa.setPeso(110.0f);
+        pessoa.setAltura(1.70f);
+        pessoa.setSexo("Masculino");
+        assertEquals("Obeso", pessoa.verificarImc());
+    }
+
+    @Test
+    public void testSexoInvalido() {
+        try {
+            pessoa.setSexo("outro");
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("Sexo Inválido", e.getMessage());
+        }
+    }
+}
